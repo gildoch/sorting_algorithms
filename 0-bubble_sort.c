2fd48dec1554c758327f1b074d4f10cb6f225d31
Gildo Chauze <gildochauze@gmail.com>
2024-01-29 00:32:42 +0200

    fix linting 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "sort.h"
+
 /**
  * bubble_sort - function to sort
  * @array: array to sort
@@ -8,27 +8,27 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-        int tmp = 0;
-        int changed = 1;
-        size_t i;
+	int tmp = 0;
+	int changed = 1;
+	size_t i;
 
-        if (!array || !size)
-                return;
+	if (!array || !size)
+		return;
 
-        while (changed)
-        {
-                changed = 0;
+	while (changed)
+	{
+		changed = 0;
 
-                for (i = 0; i < size - 1; i++)
-                {
-                        if (array[i] > array[i + 1])
-                        {
-                                tmp = array[i];
-                                array[i] = array[i + 1];
-                                array[i + 1] = tmp;
-                                changed = 1;
-                                print_array(array, size);
-                        }
-                }
-        }
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				changed = 1;
+				print_array(array, size);
+			}
+		}
+	}
 }
