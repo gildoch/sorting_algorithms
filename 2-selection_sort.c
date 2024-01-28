443ad4d1f39ab22e7385c56fbca4cc2843f54d24
Gildo Chauze <gildochauze@gmail.com>
2024-01-28 23:17:02 +0200

    bubble sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorting algoritm
+ * @array: arrary to be sorted
+ * @size: size of the array to be sorted
+ * Return: None
+*/
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	size_t minIndex;
+	int tmp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		minIndex = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[minIndex] > array[j])
+			{
+				minIndex = j;
+			}
+		}
+
+		if (minIndex != i)
+		{
+			tmp = array[i];
+			array[i] = array[minIndex];
+			array[minIndex] = tmp;
+			print_array(array, size);
+		}
+
+
+	}
+
+
+}
